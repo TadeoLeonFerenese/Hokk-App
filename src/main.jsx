5aea88054958b7b6c09eb4d63382d413b230d3d8
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2023-07-28 14:15:27 -0300

    On fire

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HookApp } from "./HooksApp";
+import { CounterApp } from "./01-useState/CounterApp";
+// import { HookApp } from "./HooksApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HookApp />
+    <CounterApp />
   </React.StrictMode>
 );
