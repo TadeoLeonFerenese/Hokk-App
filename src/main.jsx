aefed4626e038d9116b215f387ea5ba91ac11787
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2023-08-28 19:52:05 -0300

    On Road

--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,10 @@ import "./index.css";
 // import { CallbackHook } from "./06-memos/callbackHook";
 import { Padre } from "./07-tarea-memo/Padre";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Padre />
-  // {/* </React.StrictMode> */}
-);
+import "./08-useReducer/intro-Reducer";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   // <React.StrictMode>
+//   <Padre />
+//   // {/* </React.StrictMode> */}
+// );
