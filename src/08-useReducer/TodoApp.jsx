ca6e3bc3ef8c5f94181a977598162ced284cb366
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2023-09-04 18:59:24 -0300

    On Road

---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,59 @@
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: "Recolectar la piedra del alma",
+    done: false,
+  },
+  {
+    id: new Date().getTime() * 3,
+    description: "Recolectar la piedra del alma",
+    done: false,
+  },
+];
+
+export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+  return (
+    <>
+      <h1>
+        TodoApp 10, <small>pendientes: 2</small>
+      </h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group">
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className="list-group-item d-flex justify-content-between"
+              >
+                <span className="align-self-center">Item 1</span>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="¿Que hay que hacer?"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,13 @@ import "./index.css";
 // import { Memorize } from "./06-memos/Memorize";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { CallbackHook } from "./06-memos/callbackHook";
-import { Padre } from "./07-tarea-memo/Padre";
+// import { Padre } from "./07-tarea-memo/Padre";
+// import "./08-useReducer/intro-Reducer";
 
-import "./08-useReducer/intro-Reducer";
+import { TodoApp } from "./08-useReducer/TodoApp";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   // <React.StrictMode>
-//   <Padre />
-//   // {/* </React.StrictMode> */}
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  //   // <React.StrictMode>
+  <TodoApp />
+  //   // {/* </React.StrictMode> */}
+);
