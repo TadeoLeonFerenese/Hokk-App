52ec1ee635a5cebb92beb1b031241cc0cf93d5a8
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2024-01-09 18:58:56 -0300

    El peluca sapeee

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -7,7 +7,7 @@ import { Navbar } from "./Navbar";
 export const MainApp = () => {
   return (
     <>
-      <h1>Main App</h1>
+      {/* <h1>Main App</h1> */}
       <Navbar />
       <hr />
 
--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,12 +1,45 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <>
-      {/*esto es un Ancord con esteroides, hace que cuando refreshees solo carga esta porcion de dato y no renderiza todo */}
-      <Link to="/">Home</Link>
-      <Link to="about">About</Link>
-      <Link to="login">Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          useContext
+        </Link>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {/* NavLink es un Link pero te permite agregar codigo en este caso logica para saber cuando estas parado en home o about */}
+            <NavLink
+              className={({ isActive }) =>
+                ` nav-link ${isActive ? "active" : ""}`
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                ` nav-link ${isActive ? "active" : ""}`
+              }
+              to="/about"
+            >
+              About
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) =>
+                ` nav-link ${isActive ? "active" : ""}`
+              }
+              to="/login"
+            >
+              Login
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
